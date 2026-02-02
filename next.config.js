595f0de7e14b6e4fdb71be0e692168de74b2388a
randicortexlabs <randi@cortexlabs.digital>
2026-02-02 20:06:33 +1030

    domain config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/influencer-report-generator',
-  assetPrefix: '/influencer-report-generator',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
